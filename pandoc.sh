0f35fc78c7e981c5e2433a8e50ae5a7ccd91d684
Nicolas Michel <Nicolas@vpackets.net>
2024-03-05 17:42:29 -0600

    Added Pandoc script

--- pandoc.sh
+++ pandoc.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-MARKDOWN_FILE="/Users/nmichel/Library/CloudStorage/OneDrive-SharedLibraries-Cisco/Amazon Account Team - Amazon Web Services AWS/Projects and Technical/Lab/RTP Dcloud/Documentation/AWS-DX-RTP-LAB.md"
-HTML_FILE="/Users/nmichel/Library/CloudStorage/OneDrive-SharedLibraries-Cisco/Amazon Account Team - Amazon Web Services AWS/Projects and Technical/Lab/RTP Dcloud/Documentation/AWS-DX-RTP-LAB.html"
+# Source Markdown file
+SOURCE="SOURCE.md"
 
-echo $MARKDOWN_FILE | entr pandoc -s "$MARKDOWN_FILE" -o "$HTML_FILE"
+# Destination HTML file
+DESTINATION="HTML.html"
 
+# CSS file
+CSS="style.css"
+
+# Begin of style tag
+echo '<style>' > temp_style.css
+
+# Append CSS content
+cat "$CSS" >> temp_style.css
+
+# End of style tag
+echo '</style>' >> temp_style.css
+
+# Use Pandoc to generate HTML with embedded CSS
+pandoc "$SOURCE" -s --metadata title="Your Document Title" -H temp_style.css -o "$DESTINATION"
+
+# Cleanup temporary CSS file
+rm temp_style.css
+
+
+# Alternatively, just run this command: 
+# pandoc SOURCE.md -s -o HTML.html -c style.css
